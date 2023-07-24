483440d10150a73ede397b93f3ad88485d152a02
dy0221 <wjm2517@hanyang.ac.kr>
2023-07-24 14:12:20 +0900

    Delete prefile

--- cpp_example/05-1Inline.cpp
+++
@@ -1,9 +0,0 @@
-#include "05-1Inline.h"
-
-void BestComImp1::SimpleFunc(void){
-    std::cout<<"BestComImp1이 정의한 함수"<<std::endl;
-}
-
-void ProComImp1::SimpleFunc(void){
-    std::cout<<"ProComImp1이 정의한 함수"<<std::endl;
-}
--- cpp_example/05-1main.cpp
+++
@@ -1,7 +0,0 @@
-#include "05-1Inline.h"
-
-int main(void){
-    BestComImp1::SimpleFunc();
-    ProComImp1::SimpleFunc();
-    return 0;
-}
